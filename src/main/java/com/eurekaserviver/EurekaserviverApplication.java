b80e1f2dd28f6d1d1483783178ee0a1b8498704e
Mangesh Pakhale <mangeshpakhale2016@gmail.com>
2024-05-07 23:03:00 +0530

    first commit

---
+++ src/main/java/com/eurekaserviver/EurekaserviverApplication.java
@@ -0,0 +1,13 @@
+package com.eurekaserviver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserviverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserviverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurekaserviver/EurekaserviverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserviver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
